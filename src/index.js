964832113304edce231166d87bbb30ffd52f4ce4
Andrew Craigie <a.craigie@btinternet.com>
2019-02-16 12:37:51 +0000

    Removes unused loadOrbitDefs

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ import { AppContainer } from 'react-hot-loader';
 import configureStore, { history } from './store/configureStore';
 import Root from './components/Root';
 
-import { loadOrbitDefs } from './actions/orbitDefsActions';
 import { loadAppSettings } from './actions/appSettingsActions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 
+
+
 const store = configureStore();
 store.dispatch(loadAppSettings());
-store.dispatch(loadOrbitDefs());
+//store.dispatch(loadOrbitDefs());
 
 render(
   <AppContainer>
